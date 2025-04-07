03dd38db6b5e6557dcede3ca4a27ad9f16b46289
merakesh1 <bngaram14337@gmail.com>
2025-04-07 15:08:33 +0530

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-console.log("This is my app")
+function isPrime(num) {
+    if (num <= 1) {
+        return false
+    }
+    else if (num == 2) {
+        return true
+    }
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return false
+        }
+    }
+    return true
+}
 
+let start = 10
+let end = 40
+
+for (let i = start; i <= end; i++) {
+    if (isPrime(i)) {
+        console.log(i)
+    }
+}
